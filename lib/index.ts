3d23b70aa45308a627a21a4d33f3f114cf2c364b
zartab001 <zartab001@gmail.com>
2026-08-23 13:29:11 +0500

    Wire up barrel exports for staff compliance, training, availability, performance features

--- lib/index.ts
+++ lib/index.ts
@@ -2,3 +2,5 @@ export * from "./get-query-client";
 export * from "./utils";
 export * from "./hooks";
 export * from "./api/client";
+export * from "./mock/staff-mock-data";
+export * from "./mock/staff-compliance-mock";
