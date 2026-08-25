c0ebefc37a7645c92aaea06c952e89ea2ec8ba1f
zartab001 <zartab001@gmail.com>
2026-08-25 08:46:18 +0500

    feat: undo changes

---
+++ components/sections/staff/index.ts
@@ -0,0 +1,6 @@
+export * from "./AvailabilityTab";
+export * from "./PerformanceTab";
+export * from "./StaffTable";
+export * from "./TrainingMatrixTab";
+export * from "./ComplianceIndicator";
+export * from "./EditStaffModal";
