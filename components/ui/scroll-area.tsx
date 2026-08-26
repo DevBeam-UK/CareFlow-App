fdceb057e636da190f2299e045fc7eeff1964b81
Afzaad Khan <afzaadkhann@gmail.com>
2026-08-26 22:06:35 +0500

    fix(dashboard): fix bugs in patient page and scheduling page

--- components/ui/scroll-area.tsx
+++ components/ui/scroll-area.tsx
@@ -3,11 +3,9 @@
 import * as React from "react"
 import { ScrollArea as ScrollAreaPrimitive } from "@base-ui/react/scroll-area"
 
-<<<<<<< HEAD
+
 import { cn } from "lib"
-=======
-import { cn } from "lib"
->>>>>>> feat/patients-page
+
 
 function ScrollArea({
   className,
